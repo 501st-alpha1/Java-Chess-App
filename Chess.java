2f2b92b19619a215d1852aa18da5493befaad7f0
Scott Weldon <public@scott-weldon.com>
2011-09-12 19:52:30 -0700

    Most functionality separated from GUI.

--- Chess.java
+++ Chess.java
@@ -1,17 +1,17 @@
 import java.awt.Color;
+import java.awt.Graphics;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import javax.swing.*;
 
-public class Chess extends JApplet {
-  Checkerboard p1;
+public class Chess extends JPanel {
+  Game p1;
   
   //Set up frame or applet. TODO: Applet doesn't work with object tag.
   public static void main(String[] args) {
     JFrame frame = new JFrame();
-    Chess applet = new Chess();
-    applet.init();
-    frame.add(applet);
+    Chess panel = new Chess();
+    frame.add(panel);
     frame.setSize(500, 500);
     frame.setLocationRelativeTo(null);
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -27,7 +27,7 @@ public class Chess extends JApplet {
 //          p1 = new Checkerboard();
 //          add(p1);
 //          p1.addMouseListener(new MoveListener());
-//        }
+//        }this.side
 //      });
 //    }
 //    catch (Exception e) {
@@ -37,9 +37,316 @@ public class Chess extends JApplet {
 
   //Create new checkerboard panel; add it and listener to frame.
   public Chess() {
-    p1 = new Checkerboard();
+    p1 = new Game();
     this.add(p1);
-    p1.addMouseListener(new MoveListener());
+    this.addMouseListener(new MoveListener());
+  }
+  
+  //If the mouse clicked on a square, process it.
+  public void processClick(int xPoint, int yPoint) {
+    int x = xPoint / p1.getSide() + 1; //Get square number for x, starting at 1
+    int y = yPoint / p1.getSide() + 1; //Get square number for y, starting at 1
+    
+    if ((x > 8) || (y > 8)) {
+      p1.setCurrentX(0);
+      p1.setCurrentY(0);
+    }
+    else if ((x == p1.getCurrentX()) && (y == p1.getCurrentY())) {
+      p1.setCurrentX(0);
+      p1.setCurrentY(0);
+    }
+    else if ((p1.white.king.getX() == p1.getCurrentX()) && (p1.white.king.getY() == p1.getCurrentY())) {
+      p1.processMove(p1.white.king, x, y, p1.white);
+    }
+    else if ((p1.white.queen.getX() == p1.getCurrentX()) && (p1.white.queen.getY() == p1.getCurrentY())) {
+      p1.processMove(p1.white.queen, x, y, p1.white);
+    }
+    else if ((p1.white.rookWest.getX() == p1.getCurrentX()) && (p1.white.rookWest.getY() == p1.getCurrentY())) {
+      p1.processMove(p1.white.rookWest, x, y, p1.white);
+    }
+    else if ((p1.white.rookEast.getX() == p1.getCurrentX()) && (p1.white.rookEast.getY() == p1.getCurrentY())) {
+      p1.processMove(p1.white.rookEast, x, y, p1.white);
+    }
+    else if ((p1.white.knightWest.getX() == p1.getCurrentX()) && (p1.white.knightWest.getY() == p1.getCurrentY())) {
+      p1.processMove(p1.white.knightWest, x, y, p1.white);
+    }
+    else if ((p1.white.knightEast.getX() == p1.getCurrentX()) && (p1.white.knightEast.getY() == p1.getCurrentY())) {
+      p1.processMove(p1.white.knightEast, x, y, p1.white);
+    }
+    else if ((p1.white.bishopWest.getX() == p1.getCurrentX()) && (p1.white.bishopWest.getY() == p1.getCurrentY())) {
+      p1.processMove(p1.white.bishopWest, x, y, p1.white);
+    }
+    else if ((p1.white.bishopEast.getX() == p1.getCurrentX()) && (p1.white.bishopEast.getY() == p1.getCurrentY())) {
+      p1.processMove(p1.white.bishopEast, x, y, p1.white);
+    }
+    else if ((p1.white.pawn1.getX() == p1.getCurrentX()) && (p1.white.pawn1.getY() == p1.getCurrentY())) {
+      p1.processMove(p1.white.pawn1, x, y, p1.white);
+    }
+    else if ((p1.white.pawn2.getX() == p1.getCurrentX()) && (p1.white.pawn2.getY() == p1.getCurrentY())) {
+      p1.processMove(p1.white.pawn2, x, y, p1.white);
+    }
+    else if ((p1.white.pawn3.getX() == p1.getCurrentX()) && (p1.white.pawn3.getY() == p1.getCurrentY())) {
+      p1.processMove(p1.white.pawn3, x, y, p1.white);
+    }
+    else if ((p1.white.pawn4.getX() == p1.getCurrentX()) && (p1.white.pawn4.getY() == p1.getCurrentY())) {
+      p1.processMove(p1.white.pawn4, x, y, p1.white);
+    }
+    else if ((p1.white.pawn5.getX() == p1.getCurrentX()) && (p1.white.pawn5.getY() == p1.getCurrentY())) {
+      p1.processMove(p1.white.pawn5, x, y, p1.white);
+    }
+    else if ((p1.white.pawn6.getX() == p1.getCurrentX()) && (p1.white.pawn6.getY() == p1.getCurrentY())) {
+      p1.processMove(p1.white.pawn6, x, y, p1.white);
+    }
+    else if ((p1.white.pawn7.getX() == p1.getCurrentX()) && (p1.white.pawn7.getY() == p1.getCurrentY())) {
+      p1.processMove(p1.white.pawn7, x, y, p1.white);
+    }
+    else if ((p1.white.pawn8.getX() == p1.getCurrentX()) && (p1.white.pawn8.getY() == p1.getCurrentY())) {
+      p1.processMove(p1.white.pawn8, x, y, p1.white);
+    }
+    
+    else if ((p1.black.king.getX() == p1.getCurrentX()) && (p1.black.king.getY() == p1.getCurrentY())) {
+      p1.processMove(p1.black.king, x, y, p1.black);
+    }
+    else if ((p1.black.queen.getX() == p1.getCurrentX()) && (p1.black.queen.getY() == p1.getCurrentY())) {
+      p1.processMove(p1.black.queen, x, y, p1.black);
+    }
+    else if ((p1.black.rookWest.getX() == p1.getCurrentX()) && (p1.black.rookWest.getY() == p1.getCurrentY())) {
+      p1.processMove(p1.black.rookWest, x, y, p1.black);
+    }
+    else if ((p1.black.rookEast.getX() == p1.getCurrentX()) && (p1.black.rookEast.getY() == p1.getCurrentY())) {
+      p1.processMove(p1.black.rookEast, x, y, p1.black);
+    }
+    else if ((p1.black.knightWest.getX() == p1.getCurrentX()) && (p1.black.knightWest.getY() == p1.getCurrentY())) {
+      p1.processMove(p1.black.knightWest, x, y, p1.black);
+    }
+    else if ((p1.black.knightEast.getX() == p1.getCurrentX()) && (p1.black.knightEast.getY() == p1.getCurrentY())) {
+      p1.processMove(p1.black.knightEast, x, y, p1.black);
+    }
+    else if ((p1.black.bishopWest.getX() == p1.getCurrentX()) && (p1.black.bishopWest.getY() == p1.getCurrentY())) {
+      p1.processMove(p1.black.bishopWest, x, y, p1.black);
+    }
+    else if ((p1.black.bishopEast.getX() == p1.getCurrentX()) && (p1.black.bishopEast.getY() == p1.getCurrentY())) {
+      p1.processMove(p1.black.bishopEast, x, y, p1.black);
+    }
+    else if ((p1.black.pawn1.getX() == p1.getCurrentX()) && (p1.black.pawn1.getY() == p1.getCurrentY())) {
+      p1.processMove(p1.black.pawn1, x, y, p1.black);
+    }
+    else if ((p1.black.pawn2.getX() == p1.getCurrentX()) && (p1.black.pawn2.getY() == p1.getCurrentY())) {
+      p1.processMove(p1.black.pawn2, x, y, p1.black);
+    }
+    else if ((p1.black.pawn3.getX() == p1.getCurrentX()) && (p1.black.pawn3.getY() == p1.getCurrentY())) {
+      p1.processMove(p1.black.pawn3, x, y, p1.black);
+    }
+    else if ((p1.black.pawn4.getX() == p1.getCurrentX()) && (p1.black.pawn4.getY() == p1.getCurrentY())) {
+      p1.processMove(p1.black.pawn4, x, y, p1.black);
+    }
+    else if ((p1.black.pawn5.getX() == p1.getCurrentX()) && (p1.black.pawn5.getY() == p1.getCurrentY())) {
+      p1.processMove(p1.black.pawn5, x, y, p1.black);
+    }
+    else if ((p1.black.pawn6.getX() == p1.getCurrentX()) && (p1.black.pawn6.getY() == p1.getCurrentY())) {
+      p1.processMove(p1.black.pawn6, x, y, p1.black);
+    }
+    else if ((p1.black.pawn7.getX() == p1.getCurrentX()) && (p1.black.pawn7.getY() == p1.getCurrentY())) {
+      p1.processMove(p1.black.pawn7, x, y, p1.black);
+    }
+    else if ((p1.black.pawn8.getX() == p1.getCurrentX()) && (p1.black.pawn8.getY() == p1.getCurrentY())) {
+      p1.processMove(p1.black.pawn8, x, y, p1.black);
+    }
+    else {
+      p1.setCurrentX(x);
+      p1.setCurrentY(y);
+    }
+    //There must be *some* better way to do that!
+    
+    repaint();
+  }
+  
+  //Paint the checkerboard and the pieces.
+  protected void paintComponent(Graphics g) {
+    super.paintComponent(g);
+    
+    //Get the width of each square
+    if (this.getWidth() > this.getHeight()) {
+      p1.setSide(this.getHeight() / 8);
+    }
+    else {
+      p1.setSide(this.getWidth() / 8);
+    }
+    
+    //Draw squares
+    boolean isWhite = true;
+    for (int i = 0; i < 8; i++) {
+      for (int j = 0; j < 8; j++) {
+        if (isWhite) {
+          g.setColor(Color.WHITE);
+          isWhite = false;
+        }
+        else {
+          g.setColor(new Color(20, 190, 20));
+          isWhite = true;
+        }
+        g.fillRect((i * p1.getSide()), (j * p1.getSide()), (p1.getSide()), (p1.getSide()));
+      }
+      
+      if (i % 2 == 0) {
+        isWhite = false;
+      }
+      else {
+        isWhite = true;
+      }
+    }
+    
+    if ((p1.getCurrentX() != 0) && (p1.getCurrentY() != 0)) {
+      g.setColor(Color.RED);
+      g.fillRect((p1.getCurrentX() - 1) * p1.getSide(), (p1.getCurrentY() - 1) * p1.getSide(),
+          p1.getSide(), p1.getSide());
+    }
+    
+    //Make sure everything is the right size
+    Piece.setWidth(p1.getSide());
+    p1.white.king.setPolygon();
+    p1.white.queen.setPolygon();
+    p1.white.rookWest.setPolygon();
+    p1.white.rookEast.setPolygon();
+    p1.white.knightWest.setPolygon();
+    p1.white.knightEast.setPolygon();
+    p1.white.bishopWest.setPolygon();
+    p1.white.bishopEast.setPolygon();
+    p1.white.pawn1.setPolygon();
+    p1.white.pawn2.setPolygon();
+    p1.white.pawn3.setPolygon();
+    p1.white.pawn4.setPolygon();
+    p1.white.pawn5.setPolygon();
+    p1.white.pawn6.setPolygon();
+    p1.white.pawn7.setPolygon();
+    p1.white.pawn8.setPolygon();
+    
+    p1.black.king.setPolygon();
+    p1.black.queen.setPolygon();
+    p1.black.rookWest.setPolygon();
+    p1.black.rookEast.setPolygon();
+    p1.black.knightWest.setPolygon();
+    p1.black.knightEast.setPolygon();
+    p1.black.bishopWest.setPolygon();
+    p1.black.bishopEast.setPolygon();
+    p1.black.pawn1.setPolygon();
+    p1.black.pawn2.setPolygon();
+    p1.black.pawn3.setPolygon();
+    p1.black.pawn4.setPolygon();
+    p1.black.pawn5.setPolygon();
+    p1.black.pawn6.setPolygon();
+    p1.black.pawn7.setPolygon();
+    p1.black.pawn8.setPolygon();
+    
+    //Draw the pieces
+    g.setColor(Color.BLACK);
+    if (p1.white.king.getStatus()) {
+      g.fillPolygon(p1.white.king.piece);
+    }
+    else {
+      System.out.println("Black is the winner!");
+      System.exit(0);
+    }
+    if (p1.white.queen.getStatus()) {
+      g.fillPolygon(p1.white.queen.piece);
+    }
+    if (p1.white.rookWest.getStatus()) {
+      g.fillPolygon(p1.white.rookWest.piece);
+    }
+    if (p1.white.rookEast.getStatus()) {
+      g.fillPolygon(p1.white.rookEast.piece);
+    }
+    if (p1.white.knightWest.getStatus()) {
+      g.fillPolygon(p1.white.knightWest.piece);
+    }
+    if (p1.white.knightEast.getStatus()) {
+      g.fillPolygon(p1.white.knightEast.piece);
+    }
+    if (p1.white.bishopWest.getStatus()) {
+      g.fillPolygon(p1.white.bishopWest.piece);
+    }
+    if (p1.white.bishopEast.getStatus()) {
+      g.fillPolygon(p1.white.bishopEast.piece);
+    }
+    if (p1.white.pawn1.getStatus()) {
+      g.fillPolygon(p1.white.pawn1.piece);
+    }
+    if (p1.white.pawn2.getStatus()) {
+      g.fillPolygon(p1.white.pawn2.piece);
+    }
+    if (p1.white.pawn3.getStatus()) {
+      g.fillPolygon(p1.white.pawn3.piece);
+    }
+    if (p1.white.pawn4.getStatus()) {
+      g.fillPolygon(p1.white.pawn4.piece);
+    }
+    if (p1.white.pawn5.getStatus()) {
+      g.fillPolygon(p1.white.pawn5.piece);
+    }
+    if (p1.white.pawn6.getStatus()) {
+      g.fillPolygon(p1.white.pawn6.piece);
+    }
+    if (p1.white.pawn7.getStatus()) {
+      g.fillPolygon(p1.white.pawn7.piece);
+    }
+    if (p1.white.pawn8.getStatus()) {
+      g.fillPolygon(p1.white.pawn8.piece);
+    }
+    
+    g.setColor(Color.BLUE);
+    if (p1.black.king.getStatus()) {
+      g.fillPolygon(p1.black.king.piece);
+    }
+    else {
+      System.out.println("White is the winner!");
+      System.exit(0);
+    }
+    if (p1.black.queen.getStatus()) {
+      g.fillPolygon(p1.black.queen.piece);
+    }
+    if (p1.black.rookWest.getStatus()) {
+      g.fillPolygon(p1.black.rookWest.piece);
+    }
+    if (p1.black.rookEast.getStatus()) {
+      g.fillPolygon(p1.black.rookEast.piece);
+    }
+    if (p1.black.knightWest.getStatus()) {
+      g.fillPolygon(p1.black.knightWest.piece);
+    }
+    if (p1.black.knightEast.getStatus()) {
+      g.fillPolygon(p1.black.knightEast.piece);
+    }
+    if (p1.black.bishopWest.getStatus()) {
+      g.fillPolygon(p1.black.bishopWest.piece);
+    }
+    if (p1.black.bishopEast.getStatus()) {
+      g.fillPolygon(p1.black.bishopEast.piece);
+    }
+    if (p1.black.pawn1.getStatus()) {
+      g.fillPolygon(p1.black.pawn1.piece);
+    }
+    if (p1.black.pawn2.getStatus()) {
+      g.fillPolygon(p1.black.pawn2.piece);
+    }
+    if (p1.black.pawn3.getStatus()) {
+      g.fillPolygon(p1.black.pawn3.piece);
+    }
+    if (p1.black.pawn4.getStatus()) {
+      g.fillPolygon(p1.black.pawn4.piece);
+    }
+    if (p1.black.pawn5.getStatus()) {
+      g.fillPolygon(p1.black.pawn5.piece);
+    }
+    if (p1.black.pawn6.getStatus()) {
+      g.fillPolygon(p1.black.pawn6.piece);
+    }
+    if (p1.black.pawn7.getStatus()) {
+      g.fillPolygon(p1.black.pawn7.piece);
+    }
+    if (p1.black.pawn8.getStatus()) {
+      g.fillPolygon(p1.black.pawn8.piece);
+    }
+    //There must be a better way to do that too.
   }
   
   class MoveListener implements MouseListener{
@@ -47,7 +354,7 @@ public class Chess extends JApplet {
     public void mouseClicked(MouseEvent e) {
       int x = e.getX();
       int y = e.getY();
-      p1.processClick(x, y);
+      processClick(x, y);
     }
 
     public void mouseEntered(MouseEvent arg0) {
--- Game.java
+++ Game.java
@@ -1,4 +1,201 @@
-public class Game {
-  //An array to keep track of which piece is on which square. "null" means the square is empty.
-  static Piece[][] board = new Piece[8][8];
+import java.awt.Color;
+import java.awt.Graphics;
+import javax.swing.JPanel;
+
+class Game extends JPanel {
+  public Team white = new Team(Team.WHITE);
+  public Team black = new Team(Team.BLACK);
+  private int side;
+  private int currentX = 0;
+  private int currentY = 0;
+  private Team currentTeam = white;
+  private Piece[][] board = new Piece[8][8];
+  //An array to keep track of which piece is on which square. "null" means the
+  //square is empty.
+  
+  public Game() {
+    //Initialize piece positions.
+    this.board[0][0] = black.rookEast;
+    this.board[1][0] = black.knightEast;
+    this.board[2][0] = black.bishopEast;
+    this.board[3][0] = black.queen;
+    this.board[4][0] = black.king;
+    this.board[5][0] = black.bishopWest;
+    this.board[6][0] = black.knightWest;
+    this.board[7][0] = black.rookWest;
+    this.board[0][1] = black.pawn1;
+    this.board[1][1] = black.pawn2;
+    this.board[2][1] = black.pawn3;
+    this.board[3][1] = black.pawn4;
+    this.board[4][1] = black.pawn5;
+    this.board[5][1] = black.pawn6;
+    this.board[6][1] = black.pawn7;
+    this.board[7][1] = black.pawn8;
+    
+    this.board[0][7] = white.rookWest;
+    this.board[1][7] = white.knightWest;
+    this.board[2][7] = white.bishopWest;
+    this.board[3][7] = white.queen;
+    this.board[4][7] = white.king;
+    this.board[5][7] = white.bishopEast;
+    this.board[6][7] = white.knightEast;
+    this.board[7][7] = white.rookEast;
+    this.board[0][6] = white.pawn1;
+    this.board[1][6] = white.pawn2;
+    this.board[2][6] = white.pawn3;
+    this.board[3][6] = white.pawn4;
+    this.board[4][6] = white.pawn5;
+    this.board[5][6] = white.pawn6;
+    this.board[6][6] = white.pawn7;
+    this.board[7][6] = white.pawn8;
+  }
+  
+  //Make sure the move is valid, and execute. This need improvement too.
+  public void processMove(Piece p, int x, int y, Team team) {
+    if ((team == white) && (currentTeam == white)) {
+      if ((p instanceof Pawn) &&
+          (p.getValidSquares(currentX, currentY, x, y, team, this.board))
+          && (y == 1)) {
+        //Upgrade a pawn?
+        p.setX(x);
+        p.setY(y);
+        this.board[x - 1][y - 1] = p;
+        this.board[currentX - 1][currentY - 1] = null;
+        currentX = 0;
+        currentY = 0;
+        currentTeam = black;
+        this.upgradePawn(p);
+      }
+      else if (p.getValidSquares(currentX, currentY, x, y, team, this.board)) {
+        p.setX(x);
+        p.setY(y);
+        this.board[x - 1][y - 1] = p;
+        this.board[currentX - 1][currentY - 1] = null;
+        currentX = 0;
+        currentY = 0;
+        currentTeam = black;
+      }
+      else if (p.getAttackSquares(currentX, currentY, x, y, team, this.board)) {
+        this.board[x - 1][y - 1].setStatus(false);
+        p.setX(x);
+        p.setY(y);
+        this.board[x - 1][y - 1] = p;
+        this.board[currentX - 1][currentY - 1] = null;
+        currentX = 0;
+        currentY = 0;
+        currentTeam = black;
+      }
+      else {
+        currentX = 0;
+        currentY = 0;
+      }
+    }
+    else if ((team == black) && (currentTeam == black)){
+      if (p.getValidSquares(currentX, currentY, x, y, team, this.board)) {
+        p.setX(x);
+        p.setY(y);
+        this.board[x - 1][y - 1] = p;
+        this.board[currentX - 1][currentY - 1] = null;
+        currentX = 0;
+        currentY = 0;
+        currentTeam = white;
+      }
+      else if (p.getAttackSquares(currentX, currentY, x, y, team, this.board)) {
+        this.board[x - 1][y - 1].setStatus(false);
+        p.setX(x);
+        p.setY(y);
+        this.board[x - 1][y - 1] = p;
+        this.board[currentX - 1][currentY - 1] = null;
+        currentX = 0;
+        currentY = 0;
+        currentTeam = white;
+      }
+      else {
+        currentX = 0;
+        currentY = 0;
+      }
+    }
+    else {
+      currentX = 0;
+      currentY = 0;
+    }
+  }
+
+  //Promote pawns to queens.
+  public void upgradePawn(Piece p) {
+    if (p == white.pawn1) {
+      white.pawn1 = new Queen(p.getX(), p.getY(), Piece.getWidth(), white.color);
+    }
+    else if (p == white.pawn2) {
+      white.pawn2 = new Queen(p.getX(), p.getY(), Piece.getWidth(), white.color);
+    }
+    else if (p == white.pawn3) {
+      white.pawn3 = new Queen(p.getX(), p.getY(), Piece.getWidth(), white.color);
+    }
+    else if (p == white.pawn4) {
+      white.pawn4 = new Queen(p.getX(), p.getY(), Piece.getWidth(), white.color);
+    }
+    else if (p == white.pawn5) {
+      white.pawn5 = new Queen(p.getX(), p.getY(), Piece.getWidth(), white.color);
+    }
+    else if (p == white.pawn6) {
+      white.pawn6 = new Queen(p.getX(), p.getY(), Piece.getWidth(), white.color);
+    }
+    else if (p == white.pawn7) {
+      white.pawn7 = new Queen(p.getX(), p.getY(), Piece.getWidth(), white.color);
+    }
+    else if (p == white.pawn8) {
+      white.pawn8 = new Queen(p.getX(), p.getY(), Piece.getWidth(), white.color);
+    }
+    else if (p == black.pawn1) {
+      black.pawn1 = new Queen(p.getX(), p.getY(), Piece.getWidth(), black.color);
+    }
+    else if (p == black.pawn2) {
+      black.pawn2 = new Queen(p.getX(), p.getY(), Piece.getWidth(), black.color);
+    }
+    else if (p == black.pawn3) {
+      black.pawn3 = new Queen(p.getX(), p.getY(), Piece.getWidth(), black.color);
+    }
+    else if (p == black.pawn4) {
+      black.pawn4 = new Queen(p.getX(), p.getY(), Piece.getWidth(), black.color);
+    }
+    else if (p == black.pawn5) {
+      black.pawn5 = new Queen(p.getX(), p.getY(), Piece.getWidth(), black.color);
+    }
+    else if (p == black.pawn6) {
+      black.pawn6 = new Queen(p.getX(), p.getY(), Piece.getWidth(), black.color);
+    }
+    else if (p == black.pawn7) {
+      black.pawn7 = new Queen(p.getX(), p.getY(), Piece.getWidth(), black.color);
+    }
+    else {
+      black.pawn8 = new Queen(p.getX(), p.getY(), Piece.getWidth(), black.color);
+    }
+  }
+  
+  //this.board[x - 1][y - 1]
+  
+  public void setCurrentX(int x) {
+    this.currentX = x;
+  }
+  
+  public int getCurrentX() {
+    return this.currentX;
+  }
+
+  public void setCurrentY(int y) {
+    this.currentY = y;
+  }
+  
+  public int getCurrentY() {
+    return this.currentY;
+  }
+  
+  public void setSide(int side) {
+    this.side = side;
+  }
+  
+  public int getSide() {
+    return this.side;
+  }
 }
